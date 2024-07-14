433745adb33b3231926f69991398e5c479a2be31
Jordy Fontoura <jordyfontoura@outlook.com>
2024-07-14 13:03:04 -0300

    feat: Refactor createResult function and add then method to IResult interface

--- packages/helpers/src/result/index.ts
+++ packages/helpers/src/result/index.ts
@@ -5,27 +5,33 @@ interface IResultOps<T, E> {
   orDefault: (defaultValue: T) => T;
   orElse: <U = T>(fn: (error: E) => U) => T | U;
   orThrow: (message?: string) => T;
+  then: <U>(fn: (value: T) => U) => IResult<U, E>;
 }
-type IResult<T, E> = (IResultData<T, undefined, false> | IResultData<undefined, E, true>) & IResultOps<T, E>;
+export type IResult<T, E> = (IResultData<T, undefined, false> | IResultData<undefined, E, true>) & IResultOps<T, E>;
 
-type IResultSuccess<T, E=unknown> = IResultData<T, undefined, false> & IResultOps<T, E>;
-type IResultError<E,T=unknown> = IResultData<undefined, E, true> & IResultOps<T, E>;
+export type IResultSuccess<T, E=unknown> = IResultData<T, undefined, false> & IResultOps<T, E>;
+export type IResultError<E,T=unknown> = IResultData<undefined, E, true> & IResultOps<T, E>;
 
 function createResult<T, E>(value: undefined, error: E): IResultError<E>;
 function createResult<T, E>(value: T, error: undefined): IResultSuccess<T>;
 function createResult<T, E>(value: T, error: E): IResult<T, E> {
+  let result: IResult<T, E>;
+
   const ops = {
     orDefault,
     orElse,
     orThrow,
+    then,
   }
   if (error) {
-    return Object.assign([undefined, error, true], ops) as IResult<T, E>;
+    result = Object.assign([undefined, error, true], ops) as IResult<T, E>;
+  } else {
+    result = Object.assign([value, undefined, false], ops) as IResult<T, E>;
   }
 
-  return Object.assign([value, undefined, false], ops) as IResult<T, E>;
+  return result;
 
-  function orDefault(defaultValue: T): T {
+  function orDefault<U = T>(defaultValue: U): T | U {
     if (error) {
       return defaultValue;
     }
@@ -48,25 +54,32 @@ function createResult<T, E>(value: T, error: E): IResult<T, E> {
 
     return value;
   }
-}
 
-function success<T>(value: T): IResultSuccess<T> {
-  return createResult(value, undefined);
-}
+  function then<U>(fn: (value: T) => U): IResultSuccess<U, E> | IResultError<E, U> {
+    if (error !== undefined) {
+      return result as unknown as IResultError<E, U>;
+    }
 
-function error<T>(error: T): IResultError<T> {
-  return createResult(undefined, error);
-}
-
-interface Promise<T> {
-  asResult<E=Error, U=T>(): Promise<IResult<U, E>>;
-}
-
-Promise.prototype.asResult = async function asResult<E=Error, U=unknown>(): Promise<IResult<U, E>> {
-  try {
-    const value = await this;
-    return success<U>(value) as IResult<U, E>;
-  } catch (error) {
-    return error(error);
+    return success(fn(value)) as IResultSuccess<U, E>;
   }
+}
+
+export function success<T, E=unknown>(value: T): IResultSuccess<T, E> {
+  return createResult<T, E>(value, undefined) as IResultSuccess<T, E>;
+}
+
+export function error<E, T=unknown>(error: E): IResultError<E, T> {
+  return createResult<T, E>(undefined, error) as IResultError<E, T>;
+}
+
+
+declare global {
+  export interface Promise<T> {
+    asResult<U=T,E=Error>(): Promise<IResult<U, E>>;
+  }
+}
+
+Promise.prototype.asResult = function asResult<E=Error, U=unknown>(): Promise<IResult<U, E>> {
+  return this.then(success, error) as Promise<IResult<U, E>>;
 };
+
---
+++ packages/helpers/src/result/result.spec.ts
@@ -0,0 +1,105 @@
+import {success, error, IResult} from '.';
+
+
+describe('Result', () => {
+  it('should create a success result', () => {
+    const [value, reason, isError] = success(1);
+
+    expect(value).toBe(1);
+    expect(reason).toBeUndefined();
+    expect(isError).toBeFalsy();
+  });
+
+  it('should create an error result', () => {
+    const [value, reason, isError] = error('error');
+
+    expect(value).toBeUndefined();
+    expect(reason).toBe('error');
+    expect(isError).toBeTruthy();
+  });
+
+  it('should not return the default value', () => {
+    const result = success(1);
+
+    expect(result.orDefault(2)).toBe(1);
+  });
+
+  it('should return the default value', () => {
+    const result = error('error');
+
+    expect(result.orDefault(2)).toBe(2);
+  });
+
+  it('should return the value', () => {
+    const result = success(1);
+
+    expect(result.orElse(() => 2)).toBe(1);
+  });
+
+  it('should return the default value', () => {
+    const result = error('error');
+
+    expect(result.orElse(() => 2)).toBe(2);
+  });
+
+  it('should throw an error', () => {
+    const result = error('error');
+
+    expect(() => result.orThrow()).toThrow('error');
+  });
+
+  it('should throw an error with custom message', () => {
+    const result = error('error');
+
+    expect(() => result.orThrow('custom')).toThrow('custom');
+  });
+
+  it('should map the value', () => {
+    const result = success(1);
+    const mapped = result.then((value) => value + 1);
+
+    expect(mapped[0]).toBe(2);
+    expect(mapped[1]).toBeUndefined();
+    expect(mapped[2]).toBeFalsy();
+  });
+
+  it('should map the error', () => {
+    const result = error<string, number>('error');
+    const mapped = result.then((value) => value + 1);
+
+    expect(mapped[0]).toBeUndefined();
+    expect(mapped[1]).toBe('error');
+    expect(mapped[2]).toBeTruthy();
+  });
+
+  it('should create a random result', () => {
+    const [value, reason, isError] = Math.random() > 0.5 ? success(1) : error('error');
+
+    if (isError) {
+      expect(value).toBeUndefined();
+      expect(reason).toBeDefined();
+      expect(isError).toBeTruthy();
+      return;
+    }
+
+    expect(value).toBe(1);
+    expect(reason).toBeUndefined();
+    expect(isError).toBeFalsy();
+  });
+
+  it('should turn promise into result', async () => {
+    async function fn(n: number): Promise<number> {
+      if (n === 0) {
+        throw new Error('error');
+      }
+
+      return 17 / n;
+    }
+
+    const [value, reason, isError] = await fn(2).asResult();
+
+    expect(value).toBe(17);
+    expect(reason).toBeUndefined();
+    expect(isError).toBeFalsy();
+  });
+});
